4b21b1b7691f8df2cc4a46cd92cb4519b8176f10
vishaln04 <vishalnagarale@gmail.com>
2017-10-01 17:33:35 +0530

    absolute diff of images

--- Multiple_images.m
+++ Multiple_images.m
@@ -4,31 +4,31 @@ close all;
 
 % Loading the first Image
 img_boat = imread('boat.png');
-subplot(2,3,1);
+subplot(2,4,1);
 imshow(img_boat);
 title('Boat Image')
 
 % Loading the second Image
 img_barbara = imread('barbara.png');
-subplot(2,3,2);
+subplot(2,4,2);
 imshow(img_barbara);
 title('Barbara Image');
 
 % Direct adding two Images
 Sum = img_barbara + img_boat;
-subplot(2,3,3);
+subplot(2,4,3);
 imshow(Sum);
 title('Direct Sum');
 
 % Average of the Images
 Avg = img_barbara/2 + img_boat/2;
-subplot(2,3,4);
+subplot(2,4,4);
 imshow(Avg);
 title('First Divide then Add');
 
 % Average_alt
 Avg_alt = Sum/2;
-subplot(2,3,5);
+subplot(2,4,5);
 imshow(Avg_alt);
 title('First add then Divide');
 
@@ -36,6 +36,13 @@ title('First add then Divide');
 function result = blend(a, b, alpha)
   result = alpha * a + (1-alpha) * b;
 endfunction
-subplot(2,3,6);
+subplot(2,4,6);
 imshow(blend(img_barbara,img_boat,0.85));
 title('Blending of Images');
+
+% Diffrence between Images
+pkg load image
+abs_diff = imabsdiff(img_barbara, img_boat);  % Order doesn't matter
+subplot(2,4,7);
+imshow(abs_diff);
+title('Absolute diff between Images');
